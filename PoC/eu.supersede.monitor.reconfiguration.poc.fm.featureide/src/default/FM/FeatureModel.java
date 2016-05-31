d720d208098aac83a288b321ad73d9e19eca7153
edith <edith@edith-l-u>
2016-05-31 19:04:52 +0200

    Create PoC models for T4.4

---
+++ PoC/eu.supersede.monitor.reconfiguration.poc.fm.featureide/src/default/FM/FeatureModel.java
@@ -0,0 +1,29 @@
+package FM;
+
+/**
+ * Variability encoding of the feature model for KeY.
+ * Auto-generated class.
+ */
+public class FeatureModel {
+
+	public static boolean high;
+	public static boolean monitoring_system;
+	public static boolean keywords;
+	public static boolean m2;
+	public static boolean social_mention_api;
+	public static boolean event1keyword2;
+	public static boolean uc_brand;
+	public static boolean event1keyword1;
+	public static boolean event2;
+	public static boolean event2keyword1;
+	public static boolean low;
+	public static boolean event2keyword2;
+	public static boolean medium;
+	public static boolean frequency;
+	public static boolean ucbrand;
+	public static boolean s_n_m_2;
+	public static boolean event1;
+	public static boolean social_networks_monitor;
+
+	
+}
