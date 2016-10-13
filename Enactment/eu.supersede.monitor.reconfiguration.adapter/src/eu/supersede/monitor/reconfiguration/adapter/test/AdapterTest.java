6ffb4ba6d9498272a30095e29bc2036242a2c23c
zavala <zavala@imagen-linux>
2016-10-13 14:42:41 +0200

    Update call to ModelRepository class

--- Enactment/eu.supersede.monitor.reconfiguration.adapter/src/eu/supersede/monitor/reconfiguration/adapter/test/AdapterTest.java
+++ Enactment/eu.supersede.monitor.reconfiguration.adapter/src/eu/supersede/monitor/reconfiguration/adapter/test/AdapterTest.java
@@ -21,15 +21,15 @@ public class AdapterTest {
 	String repository = "platform:/resource/eu.supersede.monitor.reconfiguration.models/models/";
 	String featureConfigPath = "platform:/resource/eu.supersede.monitor.reconfiguration.models/models/features/configurations/MonitoringSystemConfigDefault.yafc";
 	String featureModelPath = "platform:/resource/eu.supersede.monitor.reconfiguration.models/models/features/models/MonitoringSystem.yafm";
-	
+
 	Map<String, String> modelsLocation;
 
 	ModelRepository mr = null;
-	
+
 	URL url = null;
-	
+
 	IFeatureConfigLAO fcLAO = null;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		new StandaloneSetup().setPlatformUri("../");
@@ -39,23 +39,22 @@ public class AdapterTest {
 		modelsLocation.put("profiles", "uml_models/profiles/");
 		modelsLocation.put("patterns", "patterns/");
 		modelsLocation.put("features", "features/models/");
-		
-		mr = new ModelRepository(repository);
+
+		mr = new ModelRepository(repository,url);
 		fcLAO = new FeatureConfigLAO(new FeatureConfigDAO());
 	}
-	
+
 	@Test
 	public void validAspectLoading() {
 
 		List<Aspect> a = mr.getAspectModels("GooglePlay_API_GooglePlay", modelsLocation);
 		System.out.print(a.get(0).getFeature().getId());
-		
 
 	}
-	
+
 	/**
-	 * This test shows how from a SelectionSUPERSEDE of a FeatureConfigSUPERSEDE model its
-	 * corresponding adaptation models can be retrieved. 
+	 * This test shows how from a SelectionSUPERSEDE of a FeatureConfigSUPERSEDE
+	 * model its corresponding adaptation models can be retrieved.
 	 */
 	@Test
 	public void testGetValidFeatureAspectsForASelection() {
@@ -64,8 +63,8 @@ public class AdapterTest {
 		String featureId = fc.getSelections().get(5).getFeature().getId();
 
 		List<Aspect> a = mr.getAspectModels(featureId, modelsLocation);
-		
+
 		System.out.print(a.get(0).getFeature().getId());
 	}
-	
+
 }
